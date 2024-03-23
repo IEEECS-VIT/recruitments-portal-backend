b29f8d29e284e8f553211dcabe81122fd26c6f18
Amrit Sundarka <amritsundarka@gmail.com>
2024-03-24 00:09:01 +0530

    Fixed Submission Error and added Submission time

--- routes/responseRoute.js
+++ routes/responseRoute.js
@@ -4,18 +4,21 @@ const authenticateToken = require("./auth");
 
 const router = express.Router();
 
-router.patch('/', authenticateToken , async (req, res) => {
+router.patch('/', authenticateToken, async (req, res) => {
     const { email, domain, questions } = req.body;
 
     try {
         const existingResponse = await Response.findOneAndUpdate(
             { email: email, domain: domain },
-            { $push: { questions : { $each: questions } } },
+            {
+                // $push: { questions: { $each: questions } },
+                $set: { submissionTime: new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' } ), questions: questions }
+            },
             { new: true }
         );
 
         if (existingResponse) {
-            res.status(200).json({ message: 'Response updated successfully' });
+            res.status(200).json({ message: 'Response recorded successfully' });
         } else {
             res.status(404).json({ message: 'Response not found' });
         }
@@ -24,14 +27,14 @@ router.patch('/', authenticateToken , async (req, res) => {
     }
 });
 
-router.get('/:email',authenticateToken, (req, res) => {
+router.get('/:email', authenticateToken, (req, res) => {
     const { email } = req.params
-    Response.find({email: email})
-    .then (responses => {
-        res.status(200).json(responses)
-    }) .catch (error => {
-        res.status(500).json({message: error.message})
-    });
+    Response.find({ email: email })
+        .then(responses => {
+            res.status(200).json(responses)
+        }).catch(error => {
+            res.status(500).json({ message: error.message })
+        });
 });
 
 module.exports = router;
