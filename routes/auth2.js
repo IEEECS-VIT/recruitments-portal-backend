905f52d7944f2e2f622771f5322e5e566e5b9dd8
Amrit Sundarka <amritsundarka@gmail.com>
2024-03-28 18:47:00 +0530

    Checking from cookies

--- routes/auth2.js
+++ routes/auth2.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken2(req, res, next) {
+    const cookieHeader = req.headers.cookie;
+    const cookie = cookieHeader && cookieHeader.split('; ').find(cookie => cookie.trim().startsWith('email='));
+    
+    const userEmailFromCookie = cookie && cookie.split('=')[1];
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    
     console.log(token);
-    console.log(req.body.email);
+    console.log(userEmailFromCookie);
+    
     if (!token) {
       return res.status(401).json({ "message": "Unauthorized: Token missing" });
     }
+    
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         if (err.name === 'TokenExpiredError') {
@@ -19,12 +26,15 @@ function authenticateToken2(req, res, next) {
       if (!decoded || !decoded.email) {
         return res.status(403).json({ "message": "Forbidden: Token does not contain email" });
       }
+      
       const userEmailFromToken = decoded.email;
-      if (userEmailFromToken !== req.body.email) {
+      if (userEmailFromToken !== userEmailFromCookie) {
         return res.status(403).json({ "message": "Forbidden: Token does not match user's email" });
       }
+      
       req.user = decoded;
       next();
     });
-  }
-  module.exports = authenticateToken2;
+}
+
+module.exports = authenticateToken2;
